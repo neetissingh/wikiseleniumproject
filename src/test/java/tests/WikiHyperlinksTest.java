19b0ec67a8fe75f8a0eeaa7c826bd54477b8dd92
Neeti Singh <neetissingh@gmail.com>
2020-10-04 11:40:51 -0700

    Changed HyperLinksTest
    - Changed HyperLinks Tests
    - Formatted code
    - Changed testng.xml to run parallel tests

--- src/test/java/tests/WikiHyperlinksTest.java
+++ src/test/java/tests/WikiHyperlinksTest.java
@@ -5,6 +5,7 @@ import org.testng.AssertJUnit;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -12,52 +13,20 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-
-
-
 public class WikiHyperlinksTest extends BaseClass {
 
+	@Test(description = "the headings listed in the Contents box have functioning hyperlinks")
+	public void verifyHyperLinkstest() throws InterruptedException {
 
-	@Test(description="the headings listed in the Contents box have functioning hyperlinks")    
-	public void verifyHyperLinkstest()  throws InterruptedException {      
+		List<WebElement> hyperLinks = driver.findElements(By.xpath("//*[starts-with(@class,'toclevel-1')]/a"));
+		for (WebElement webelement : hyperLinks) {
+			webelement.click(); // click on the hyperlink
+			String currentUrl = driver.getCurrentUrl();
+			Assert.assertEquals(webelement.getAttribute("href"), currentUrl);
 
+//	 System.out.println(webelement.getAttribute("href"));
+//	 System.out.println(currentUrl);
 
-	List<WebElement> hyperLinks = driver.findElements(By.xpath("//*[starts-with(@class,'toclevel-1')]/a"));
-	for(WebElement webelement : hyperLinks)
-	{
-	 System.out.println(webelement.getAttribute("href"));
-	 driver.get(webelement.getAttribute("href")); //Get the hyperlinks
-	 List<WebElement> headerListElement1 = driver.findElements(By.xpath("//*[@class='mw-headline']"));
-	 for(WebElement webelement1 : headerListElement1) {
-		 System.out.println(driver.findElement(By.xpath("//*[@id='Family']")).isDisplayed());
-			  System.out.println(webelement1.getText());
-			  
-			}
+		}
 	}
-	
-	WebElement popup = driver.findElement(By.xpath("//a[@href='/wiki/Nike_(mythology)']"));
-	Actions action = new Actions(driver);
-	action.clickAndHold().moveToElement(popup);
-	action.moveToElement(popup).perform();
-	
-	
-	WebDriverWait wait = new WebDriverWait(driver, 30);
-	WebElement element = wait.until(
-	        ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'mwe-popups-container')]/a[2]")));
-	String actualText = element.getText();
-	
-	String expectedText = "In ancient Greek civilization, Nike was a goddess who personified victory. Her Roman equivalent was Victoria.";
-	AssertJUnit.assertEquals(expectedText, actualText);
-	System.out.println(actualText);
-	popup.click();
-	
-	
-	WebElement familyTree = driver.findElement(By.xpath("//*[@id='Family_tree']"));
-	
-	AssertJUnit.assertTrue(familyTree.isDisplayed());
-	
-	}
-	
-
-
 }
--- src/test/java/tests/WikiPopupTest.java
+++ src/test/java/tests/WikiPopupTest.java
@@ -12,70 +12,31 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-
-
-
 public class WikiPopupTest extends BaseClass {
 
+	@Test(description = "in the _Personified concepts_, `Nike` has a popup that contains the following text:")
+	public void verifyPopuptest() throws InterruptedException {
 
-	@Test(description ="the headings listed in the Contents box are used as headings on the page")          
-	public void verifyMainPagetest()  throws InterruptedException {      
-	
-	List<String> contentList = new ArrayList<String>();
-	List<String> headerList = new ArrayList<String>();
-	
-	List<WebElement> contentListElement = driver.findElements(By.xpath("//*[@class='toctext']"));
-	for(WebElement webelement : contentListElement) {
-		contentList.add(webelement.getText()); // Add the contents in the Array List
-		System.out.println(webelement.getText());
-		}
-	
-	
-	List<WebElement> headerListElement = driver.findElements(By.xpath("//*[@class='mw-headline']"));
-	for(WebElement webelement : headerListElement) {
-		headerList.add(webelement.getText());//Add the headlines in the Array List
-		  System.out.println(webelement.getText());
-		}
-	
-	AssertJUnit.assertEquals(contentList, headerList);
+		WebElement popup = driver.findElement(By.xpath("//a[@href='/wiki/Nike_(mythology)']"));
+		Actions action = new Actions(driver);
+		action.clickAndHold().moveToElement(popup);
+		action.moveToElement(popup).perform();
 
-	
-	List<WebElement> hyperLinks = driver.findElements(By.xpath("//*[starts-with(@class,'toclevel-1')]/a"));
-	for(WebElement webelement : hyperLinks)
-	{
-	 System.out.println(webelement.getAttribute("href"));
-	 driver.get(webelement.getAttribute("href"));
-	 List<WebElement> headerListElement1 = driver.findElements(By.xpath("//*[@class='mw-headline']"));
-	 for(WebElement webelement1 : headerListElement1) {
-		 System.out.println(driver.findElement(By.xpath("//*[@id='Family']")).isDisplayed());
-			  System.out.println(webelement1.getText());
-			  //System.out.println( webelement1.isDisplayed());
-			}
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		WebElement element = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//div[contains(@class,'mwe-popups-container')]/a[2]")));
+		String actualText = element.getText();
+
+		String expectedText = "In ancient Greek civilization, Nike was a goddess who personified victory. Her Roman equivalent was Victoria.";
+		AssertJUnit.assertEquals(expectedText, actualText);
+		System.out.println(actualText);
+		popup.click(); // Click on the Nike Link from the Personified concepts section 
+
+		
+		WebElement familyTree = driver.findElement(By.xpath("//*[@id='Family_tree']"));
+
+		AssertJUnit.assertTrue(familyTree.isDisplayed()); //Verify Family Tree page is displayed
+
 	}
-	
-	WebElement popup = driver.findElement(By.xpath("//a[@href='/wiki/Nike_(mythology)']"));
-	Actions action = new Actions(driver);
-	action.clickAndHold().moveToElement(popup);
-	action.moveToElement(popup).perform();
-	
-	
-	WebDriverWait wait = new WebDriverWait(driver, 30);
-	WebElement element = wait.until(
-	        ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'mwe-popups-container')]/a[2]")));
-	String actualText = element.getText();
-	
-	String expectedText = "In ancient Greek civilization, Nike was a goddess who personified victory. Her Roman equivalent was Victoria.";
-	AssertJUnit.assertEquals(expectedText, actualText);
-	System.out.println(actualText);
-	popup.click();
-	
-	
-	WebElement familyTree = driver.findElement(By.xpath("//*[@id='Family_tree']"));
-	
-	AssertJUnit.assertTrue(familyTree.isDisplayed());
-	
-	}
-	
-
 
 }
